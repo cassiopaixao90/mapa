5956e2f3368aa1d259454fc4a682c1fffd30b1fd
cassiopaixao90 <cassiop80@gmail.com>
2015-12-25 23:34:15 -0300

    autocomplete

---
+++ search.php
@@ -0,0 +1,17 @@
+<?php
+$dbHost = '127.0.0.1';
+$dbUsername = 'root';
+$dbPassword = 'admin';
+$dbName = 'codexworld';
+//connect with the database
+$db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+//get search term
+$searchTerm = $_GET['term'];
+//get matched data from skills table
+$query = $db->query("SELECT * FROM skills WHERE skill LIKE '%".$searchTerm."%' ORDER BY skill ASC");
+while ($row = $query->fetch_assoc()) {
+    $data[] = $row['skill'];
+}
+//return json data
+echo json_encode($data);
+?>
